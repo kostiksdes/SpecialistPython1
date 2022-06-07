a5e45e55847ba9a3e62247a146ecb4feae29d9a2
kostiksdes <106909981+kostiksdes@users.noreply.github.com>
2022-06-07 20:14:06 +0300

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -8,3 +8,14 @@
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
 # TODO: your code here
+
+n = int(input("На лугу пасутся: "))
+
+if (n >= 5 <= 20) and not (n % 10 == 1) or (n % 10 == 0) and not (n % 10 == 1) and (n % 10 >= 5 or n % 10 >= 9) and not (n % 10 == 1):
+    print(n, "коров")
+
+if n % 10 == 1:
+    print(n, "коровa")
+
+if (n % 10 >= 2) and (n % 10 <= 4) and not (n == 12, 13, 14):
+    print(n, "коровы")
