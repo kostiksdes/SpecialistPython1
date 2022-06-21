62c26c515dabf08166a162e024a87fa7776fada7
kostiksdes <106909981+kostiksdes@users.noreply.github.com>
2022-06-21 21:24:56 +0300

    Update and rename 01_hw_func.py to update_01_hw_func.py

--- Module4/home_work/update_01_hw_func.py
+++ Module4/home_work/update_01_hw_func.py
@@ -3,9 +3,14 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    number = str(ticket_number)
+    number_first = int(number[0]) + int(number[1])
+    number_back = int(number[-1]) + int(number[-2])
 
+    if number_first == number_back:
+        return True
+    else:
+        return False
 
 # Тестируем функцию
 print(lucky_ticket(123006))
